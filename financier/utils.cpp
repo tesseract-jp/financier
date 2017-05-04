ee53380ca1ab82239653ad0bdb2fb0e92eabc42b
Yuuki Fujita <yuuki.fujita.jp@gmail.com>
2017-05-04 23:44:09 +0900

    Add files for utils

---
+++ financier/utils.cpp
@@ -0,0 +1,9 @@
+//
+//  utils.cpp
+//  financier
+//
+//  Created by Yuuki Fujita on 2017/05/04.
+//  Copyright © 2017 Yuuki Fujita. All rights reserved.
+//
+
+#include "utils.hpp"
---
+++ financier/utils.hpp
@@ -0,0 +1,14 @@
+//
+//  utils.hpp
+//  financier
+//
+//  Created by Yuuki Fujita on 2017/05/04.
+//  Copyright © 2017 Yuuki Fujita. All rights reserved.
+//
+
+#ifndef utils_hpp
+#define utils_hpp
+
+#include <stdio.h>
+
+#endif /* utils_hpp */
