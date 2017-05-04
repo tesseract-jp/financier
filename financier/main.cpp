1d6622aadd98edba0d3fffa64c6e340f8f129269
Yuuki Fujita <yuuki.fujita.jp@gmail.com>
2017-05-04 23:30:40 +0900

    Initial Commit

---
+++ financier/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  financier
+//
+//  Created by Yuuki Fujita on 2017/05/04.
+//  Copyright © 2017 Yuuki Fujita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
