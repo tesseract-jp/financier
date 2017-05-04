058151cd9e7061595c56444eb6f636352d01322c
Yuuki Fujita <yuuki.fujita.jp@gmail.com>
2017-05-04 23:38:02 +0900

    Test

--- financier/main.cpp
+++ financier/main.cpp
@@ -7,9 +7,11 @@
 //
 
 #include <iostream>
+using std::cout;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    
     return 0;
 }
